d35573cd68862f0de1cababdaf3b6d847fc0c4bb
juliana615 <albrecht.2004.j@gmail.com>
2025-03-08 02:12:15 +0800

    added selenium scraper, but failed due to cloudflare protection

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+from crunchbase import Crunchbase
+import json
+
+def get_credentials():
+    with open('credentials.json') as file:
+        credentials = json.load(file)
+    return credentials
+
+def get_urls():
+    with open('urls.json') as file:
+        urls = json.load(file)
+
+def get_data():
+    credentials = get_credentials()
+    crunchbase_urls = get_urls()
+
+    crunchbase = Crunchbase()
+
+    crunchbase.login(email=credentials.get('email'), password=credentials.get('password'))
+
+    crunchbase_data = list()
+
+    for name, url in crunchbase_urls.items():
+        data = crunchbase.process_profile(pro=True, name=name, url=url)
+        if data is not None:
+            crunchbase_data.append(data)
+
+        # Writes the scraped data to the JSON file
+        with open('data/demo_crunchbase_data.json', 'w', newline='') as json_file:
+            json.dump(crunchbase_data, fp=json_file, indent=3, ensure_ascii=False)
+        
 def main():
-    pass
+    get_data()
 
 if __name__ == '__main__':
     main()
